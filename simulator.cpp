05a9dffef40a372863966fa82b5d9bb3e6b98356
mrain <linmrain@gmail.com>
2016-11-22 13:25:25 -0500

    Binary for windows

--- simulator.cpp
+++ simulator.cpp
@@ -5,25 +5,16 @@
 #include <cmath>
 #include <vector>
 #include <algorithm>
+#include <cstring>
 using namespace std;
 
 const int rounds = 10000;
-int freq[10];
 
 const double pi = acos(-1.);
 
-int main() {
-
-	const int n = 6;
-	float angles[n];
-	const float h = 0.3;
-	int tot = 0;
-	for (int i = 0; i < n - 1; ++ i)
-		tot += (1 << i) + (n - i);
-	for (int i = 0; i < n - 1; ++ i)
-		angles[i] = pi * 2.0 * ((1 << i) + (n - i)) / tot;
-
-	srand(time(0));
+void simulation(int n, const float *angles, const float h) {
+	int freq[n];
+	memset(freq, 0, sizeof(freq));
 	for (int i = 0; i < rounds; ++ i) {
 		if (i % 1000 == 0) cout << i << endl;
 		//cout << simulator->simulate() << endl;
@@ -58,5 +49,35 @@ int main() {
 	for (int i = 1; i < p.size() - 1; ++ i)
 		gap = max(gap, p[i + 1] - p[i]);
 	cout << "Estimated gap: " << gap << endl;
+}
+
+int main() {
+
+	srand(time(0));
+
+	const int n = 6;
+	int weight[n];
+	float angles[n];
+	float h = 0.3;
+	int tot = 0;
+	for (int i = 0; i < n - 1; ++ i)
+		tot += (1 << i) + (n - i);
+	for (int i = 0; i < n - 1; ++ i)
+		angles[i] = pi * 2.0 * ((1 << i) + (n - i)) / tot;
+
+	simulation(n, angles, h);
+
+	while (true) {
+		tot = 0;
+		for (int i = 0; i < n - 1; ++ i) {
+			cin >> weight[i];
+			tot += weight[i];
+		}
+		cin >> h;
+		for (int i = 0; i < n; ++ i) {
+			angles[i] = pi * 2.0 * weight[i] / tot;
+		}
+		simulation(n, angles, h);
+	}
 	return 0;
 }
